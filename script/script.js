7d3888eedae6993e72b789dfcd17e8014597cc5c
Сергей <sergbfmv@mail.ru>
2022-03-10 16:34:43 +0300

    second commit

--- script/script.js
+++ script/script.js
@@ -94,3 +94,83 @@ overlayVacancy.addEventListener("click", (e) => {
     overlayVacancy.classList.remove("overlay_active")
   }
 })
+
+//создание карточки
+
+function createCard(vacancy) {
+  const {
+    title,
+    id,
+    compensation,
+    workShedule,
+    employer,
+    address,
+    description,
+    date
+  } = vacancy
+  const card = document.createElement("li")
+  card.classList.add("result__item")
+
+  card.insertAdjacentHTML("afterbegin", `
+    <article class="vacancy">
+      <h2 class="vacancy__title">
+        <a class="vacancy__open-modal" href="#" data-vacancy="${id}">${title}</a>
+      </h2>
+      <p class="vacancy__compensation">${compensation}</p>
+      <p class="vacancy__work-schedule">${workShedule}</p>
+      <div class="vacancy__employer">
+        <p class="vacancy__employer-title">${employer}</p>
+        <p class="vacancy__employer-address">${address}</p>
+      </div>
+      <p class="vacancy__description">${description}</p>
+      <p class="vacancy__date">
+        <time datetime="${date}">${date}</time>
+      </p>
+      <div class="vacancy__wrapper-btn">
+        <a class="vacancy__response vacancy__open-modal" href="#" data-vacancy="${id}">Откликнуться</a>
+        <button class="vacancy__contacts">Показать контакты</button>
+      </div>
+    </article>
+  `)
+  return card
+}
+
+function renderCards(data) {
+  resultList.textContent = ""
+  const cards = data.map(createCard)
+  resultList.append(...cards)
+}
+
+const getData = ({search} = {}) => {
+  if (search) {
+    return fetch(`http://localhost:3000/api/vacancy?search=${search}`).then(res => res.json())
+  }
+  return fetch("http://localhost:3000/api/vacancy").then(res => res.json())
+  
+}
+
+const formSearch = document.querySelector(".bottom__search")
+
+formSearch.addEventListener("submit", async (e) => {
+  e.preventDefault()
+  const textSearch = formSearch.search.value
+
+  if (textSearch.length > 2) {
+    formSearch.search.style.borderColor = ""
+    const data = await getData({search: textSearch})
+    renderCards(data)
+    formSearch.reset()
+  } else {
+    formSearch.search.style.borderColor = "red"
+    setTimeout(() => {
+      formSearch.search.style.borderColor = ""
+    }, 2000)
+  }
+})
+
+const init = async () => {
+  const data = await getData()
+  renderCards(data)
+}
+
+init()
