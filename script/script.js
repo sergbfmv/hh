0a4c1e527de3804c10d4ab975f3b45152e9c1208
Сергей <sergbfmv@mail.ru>
2022-03-13 17:36:55 +0300

    third commit

--- script/script.js
+++ script/script.js
@@ -1,101 +1,27 @@
-//выпадающие списки
-
 const optionBtnOrder = document.querySelector(".option__btn_order")
 const optionBtnPeriod = document.querySelector(".option__btn_period")
 const optionListOrder = document.querySelector(".option__list_order")
 const optionListPeriod = document.querySelector(".option__list_period")
-
-optionBtnOrder.addEventListener("click", () => {
-  optionListOrder.classList.toggle("option__list_active")
-  optionListPeriod.classList.remove("option__list_active")
-})
-
-optionBtnPeriod.addEventListener("click", () => {
-  optionListPeriod.classList.toggle("option__list_active")
-  optionListOrder.classList.remove("option__list_active")
-})
-
-optionListOrder.addEventListener("click", (e) => {
-  const target = e.target
-
-  if (target.classList.contains("option__item")) {
-    optionBtnOrder.textContent = target.textContent
-    optionListOrder.classList.remove("option__list_active")
-
-    for (const elem of optionListOrder.querySelectorAll(".option__item")) {
-      if (elem === target) {
-        elem.classList.add("option__item_active")
-      } else {
-        elem.classList.remove("option__item_active")
-      }
-    }
-  }
-})
-
-optionListPeriod.addEventListener("click", (e) => {
-  const target = e.target
-  
-  if (target.classList.contains("option__item")) {
-    optionBtnPeriod.textContent = target.textContent
-    optionListPeriod.classList.remove("option__list_active")
-
-    for (const elem of optionListPeriod.querySelectorAll(".option__item")) {
-      if (elem === target) {
-        elem.classList.add("option__item_active")
-      } else {
-        elem.classList.remove("option__item_active")
-      }
-    }
-  }
-})
-
-//выбор города
-
 const topCityBtn = document.querySelector(".top__city")
 const city = document.querySelector(".city")
 const cityCloseBtn = document.querySelector(".city__close")
 const cityRegionList = document.querySelector(".city__region-list")
-
-topCityBtn.addEventListener("click", () => {
-  city.classList.toggle("city_active")
-})
-
-cityCloseBtn.addEventListener("click", () => {
-  city.classList.remove("city_active")
-})
-
-cityRegionList.addEventListener("click", (e) => {
-  const target = e.target
-
-  if (target.classList.contains("city__link")) {
-    topCityBtn.textContent = target.textContent
-    city.classList.remove("city_active")
-  }
-})
-
-//модальное окно
-
+const formSearch = document.querySelector(".bottom__search")
+const found = document.querySelector(".found")
 const overlayVacancy = document.querySelector(".overlay_vacancy")
 const resultList = document.querySelector(".result__list")
 
-resultList.addEventListener("click", (e) => {
-  const target = e.target
-
-  if (target.dataset.vacancy) {
-    e.preventDefault()
-    overlayVacancy.classList.add("overlay_active")
+const getData = ({search, id} = {}) => {
+  if (search) {
+    return fetch(`http://localhost:3000/api/vacancy?search=${search}`).then(res => res.json())
   }
-})
+  return fetch(`http://localhost:3000/api/vacancy/${id ? id : ""}`).then(res => res.json())
+}
 
-overlayVacancy.addEventListener("click", (e) => {
-  const target = e.target
-
-  if (target === overlayVacancy || target.classList.contains("modal__close")) {
-    overlayVacancy.classList.remove("overlay_active")
-  }
-})
-
-//создание карточки
+const declOfNum = (n, titles) => {
+  return n + ' ' + titles[n % 10 === 1 && n % 100 !== 11 ?
+    0 : n % 10 >= 2 && n % 10 <= 4 && (n % 100 < 10 || n % 100 >= 20) ? 1 : 2];
+}
 
 function createCard(vacancy) {
   const {
@@ -141,36 +67,210 @@ function renderCards(data) {
   resultList.append(...cards)
 }
 
-const getData = ({search} = {}) => {
-  if (search) {
-    return fetch(`http://localhost:3000/api/vacancy?search=${search}`).then(res => res.json())
-  }
-  return fetch("http://localhost:3000/api/vacancy").then(res => res.json())
+function optionHandler() {
+  optionBtnOrder.addEventListener("click", () => {
+    optionListOrder.classList.toggle("option__list_active")
+    optionListPeriod.classList.remove("option__list_active")
+  })
   
+  optionBtnPeriod.addEventListener("click", () => {
+    optionListPeriod.classList.toggle("option__list_active")
+    optionListOrder.classList.remove("option__list_active")
+  })
+  
+  optionListOrder.addEventListener("click", (e) => {
+    const target = e.target
+  
+    if (target.classList.contains("option__item")) {
+      optionBtnOrder.textContent = target.textContent
+      optionListOrder.classList.remove("option__list_active")
+  
+      for (const elem of optionListOrder.querySelectorAll(".option__item")) {
+        if (elem === target) {
+          elem.classList.add("option__item_active")
+        } else {
+          elem.classList.remove("option__item_active")
+        }
+      }
+    }
+  })
+  
+  optionListPeriod.addEventListener("click", (e) => {
+    const target = e.target
+    
+    if (target.classList.contains("option__item")) {
+      optionBtnPeriod.textContent = target.textContent
+      optionListPeriod.classList.remove("option__list_active")
+  
+      for (const elem of optionListPeriod.querySelectorAll(".option__item")) {
+        if (elem === target) {
+          elem.classList.add("option__item_active")
+        } else {
+          elem.classList.remove("option__item_active")
+        }
+      }
+    }
+  })
 }
 
-const formSearch = document.querySelector(".bottom__search")
+function cityHandler() {
+  topCityBtn.addEventListener("click", () => {
+    city.classList.toggle("city_active")
+  })
+  
+  cityCloseBtn.addEventListener("click", () => {
+    city.classList.remove("city_active")
+  })
+  
+  cityRegionList.addEventListener("click", (e) => {
+    const target = e.target
+  
+    if (target.classList.contains("city__link")) {
+      topCityBtn.textContent = target.textContent
+      city.classList.remove("city_active")
+    }
+  })
+}
 
-formSearch.addEventListener("submit", async (e) => {
-  e.preventDefault()
-  const textSearch = formSearch.search.value
+function createModal(data) {
+  const {
+    address,
+    compensation,
+    date,
+    description,
+    employer,
+    employment,
+    experience,
+    skills,
+    title
+  } = data
 
-  if (textSearch.length > 2) {
-    formSearch.search.style.borderColor = ""
-    const data = await getData({search: textSearch})
-    renderCards(data)
-    formSearch.reset()
-  } else {
-    formSearch.search.style.borderColor = "red"
-    setTimeout(() => {
-      formSearch.search.style.borderColor = ""
-    }, 2000)
+  const modal = document.createElement("div")
+  modal.classList.add("modal")
+
+  const closeBtnElem = document.createElement("button")
+  closeBtnElem.classList.add("modal__close")
+  closeBtnElem.textContent = "X"
+
+  const titleElem = document.createElement("h2")
+  titleElem.classList.add("modal__title")
+  titleElem.textContent = title
+
+  const compensationElem = document.createElement("p")
+  compensationElem.classList.add("modal__compensation")
+  compensationElem.textContent = compensation
+
+  const employerElem = document.createElement("p")
+  employerElem.classList.add("modal__employer")
+  employerElem.textContent = employer
+
+  const addressElem = document.createElement("p")
+  addressElem.classList.add("modal__address")
+  addressElem.textContent = address
+
+  const experienceElem = document.createElement("p")
+  experienceElem.classList.add("modal__experience")
+  experienceElem.textContent = experience
+
+  const employmentElem = document.createElement("p")
+  employmentElem.classList.add("modal__employment")
+  employmentElem.textContent = employment
+  
+  const descriptionElem = document.createElement("p")
+  descriptionElem.classList.add("modal__description")
+  descriptionElem.textContent = description
+
+  const skillsElem = document.createElement("div")
+  skillsElem.classList.add("modal__skills", "skills")
+
+  const skillsTitleElem = document.createElement("h3")
+  skillsTitleElem.classList.add("skills__title")
+  skillsTitleElem.textContent = "Подробнее:"
+
+  const skillsListElem = document.createElement("ul")
+  skillsListElem.classList.add("skills__list")
+
+  for (const skill of skills) {
+    const skillsItemElem = document.createElement("li")
+    skillsItemElem.classList.add("skills__item")
+    skillsItemElem.textContent = skill
+    skillsListElem.append(skillsItemElem)
   }
-})
+
+  skillsElem.append(skillsTitleElem, skillsListElem)
+
+  const submitBtnElem = document.createElement("button")
+  submitBtnElem.classList.add("modal__response")
+  submitBtnElem.textContent = "Отправить резюме"
+
+  modal.append(
+    closeBtnElem,
+    titleElem,
+    compensationElem,
+    employerElem,
+    addressElem,
+    experienceElem,
+    employmentElem,
+    descriptionElem,
+    skillsElem,
+    submitBtnElem
+  )
+  return modal
+}
+
+function resultHandler() {
+  let modal = null
+
+  resultList.addEventListener("click", async (e) => {
+    const target = e.target
+  
+    if (target.dataset.vacancy) {
+      e.preventDefault()
+      overlayVacancy.classList.add("overlay_active")
+      const data = await getData({id: target.dataset.vacancy})
+      modal = createModal(data)
+      overlayVacancy.append(modal)
+    }
+  })
+  
+  overlayVacancy.addEventListener("click", (e) => {
+    const target = e.target
+  
+    if (target === overlayVacancy || target.classList.contains("modal__close")) {
+      overlayVacancy.classList.remove("overlay_active")
+      modal.remove()
+    }
+  })
+}
+
+function searchHandler() {
+  formSearch.addEventListener("submit", async (e) => {
+    e.preventDefault()
+    const textSearch = formSearch.search.value
+  
+    if (textSearch.length > 2) {
+      formSearch.search.style.borderColor = ""
+      const data = await getData({search: textSearch})
+      found.innerHTML = `${declOfNum(data.length, ["вакансия", "вакансии", "вакансий"])} &laquo;${textSearch}&raquo;`
+      renderCards(data)
+      formSearch.reset()
+    } else {
+      formSearch.search.style.borderColor = "red"
+      setTimeout(() => {
+        formSearch.search.style.borderColor = ""
+      }, 2000)
+    }
+  })
+}
 
 const init = async () => {
   const data = await getData()
   renderCards(data)
+
+  optionHandler()
+  cityHandler()
+  resultHandler()
+  searchHandler()
 }
 
 init()
